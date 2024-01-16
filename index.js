5839764969503dbc287707ae7bc264741724d440
assadujjaman112 <assadujjamanmurad120@gmail.com>
2024-01-16 23:59:19 +0600

    completed the project

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-
-const uri =
-  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.knxp44y.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.knxp44y.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -27,44 +25,48 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const recipeCollection = client.db("recipeDB").collection("recipe");
 
-    const recipeCollection = client.db("recipeDB").collection("recipe")
+    app.get("/recipe", async (req, res) => {
+      const result = await recipeCollection.find().toArray();
+      res.send(result);
+    });
 
-
-    app.get("/recipe", async(req, res) => {
-        const result = await recipeCollection.find().toArray();
-        res.send(result)
-    })
-
-    app.get("/recipe/:id", async(req, res) => {
+    app.get("/recipe/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await recipeCollection.findOne(query);
       res.send(result);
-    })
+    });
 
-    app.post("/recipe", async(req, res)=> {
-        const recipe = req.body;
-        const result = await recipeCollection.insertOne(recipe);
-        res.send(result);
-    })
+    app.post("/recipe", async (req, res) => {
+      const recipe = req.body;
+      const result = await recipeCollection.insertOne(recipe);
+      res.send(result);
+    });
 
-    app.patch("/recipe/:id", async(req, res) => {
+    app.patch("/recipe/:id", async (req, res) => {
       const recipe = req.body;
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)};
+      const filter = { _id: new ObjectId(id) };
       const updatedRecipe = {
-        $set : {
-          title : recipe.title,
-          instruction : recipe.instruction,
-          image : recipe.image,
-          ingredients : recipe.ingredients
-        }
-      }
+        $set: {
+          title: recipe.title,
+          instruction: recipe.instruction,
+          image: recipe.image,
+          ingredients: recipe.ingredients,
+        },
+      };
       const result = await recipeCollection.updateOne(filter, updatedRecipe);
       res.send(result);
-    })
+    });
 
+    app.delete("/recipe/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await recipeCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
